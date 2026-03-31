3f1b384b33c48fcd641b0f6a71bf4f4ef569cd3d
BigDebzz <debbiescorner7@gmail.com>
2026-03-31 23:18:12 +0100

    Update ResultsDashboard.js

--- src/components/ResultsDashboard.js
+++ src/components/ResultsDashboard.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { supabase } from '../lib/supabase';
 import { modeConfig } from '../data/questions';
 import { Analytics } from '../lib/analytics';
 
